deb97a22c432241923f9b90afacccf7051514a03
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-07-03 11:06:10 +0300

    feat:added Artist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,5 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
-    end
+
 end
