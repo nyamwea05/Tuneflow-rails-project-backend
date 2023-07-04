71da4e2e42e38788320a05495f4fadc38a8915d4
Mbiki-KK <wawerumbiki@gmail.com>
2023-07-04 15:33:09 +0300

    feat: show route added

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,40 @@
 class PlaylistsController < ApplicationController
+  wrap_parameters format: []
+
+  def index
+    playlists = Playlist.all
+    render json: playlists
+  end
+
+  def create
+    playlist = Playlist.create(playlist_params)
+    if authorized
+      if playlist
+        render json: playlist
+      else
+        render json: {error: "unable to create"}, status: :unprocessable_entity
+      end
+    else
+      render json: {error: "sign it to continue"}, status: :unauthorized
+    end
+  end
+
+  def show
+    playlist = Playlist.find_by(id: params[:id])
+    if playlist
+      render json: playlist.song
+    else
+      render json: {error: "playlist not found"}, status: :not_found
+    end
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def playlist_params
+    params.permit(:playlist_name, :descriptions)
+  end
 end
