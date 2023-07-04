d6b8df56e1a4d81dfaaa2009f21ac7a64a566ff5
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-07-04 11:07:01 +0300

    feat:update user model  and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,19 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    before_action :authorized
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Unauthorized request', status: 'error' }, status: :unauthorized unless logged_in?
+  end
+
 
 end
