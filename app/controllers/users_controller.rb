b635274bce85d098f18c69eb5445c027556ec546
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-07-04 09:44:56 +0300

    feat:added users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,56 @@
 class UsersController < ApplicationController
-end
+    before_action :set_user, only: %i[ show update destroy ]
+    skip_before_action :authorized, only: [:create]
+    wrap_parameters :user, include: %i[first_name last_name phone email username bio avatar password password_confirmation]
+  
+    # GET /users
+    def index
+      @users = User.all
+  
+      render json: @users
+    end
+  
+    # GET /users/1
+    def show
+      render json: @user
+    end
+  
+    # POST /users
+    def create
+      @user = User.new(user_params)
+  
+      if @user.save
+        session[:user_id] = @user.id
+  
+        render json: { user: UserSerializer.new(@user) }, status: :created, location: @user
+      else
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /users/1
+    def update
+      if @user.update(user_params)
+        render json: UserSerializer.new(@user)
+      else
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /users/1
+    def destroy
+      @user.destroy
+    end
+  
+    private
+  
+    # Make use of private methods to avoid DRY
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    # Include only the parameters you require
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :phone, :email, :username, :bio, :avatar, :password,:password_confirmation)
+    end
+  end
