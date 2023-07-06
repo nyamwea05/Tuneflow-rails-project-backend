43312f369808ae0272e595836b45a629b49ac520
Mbiki-KK <wawerumbiki@gmail.com>
2023-07-06 23:01:18 +0300

    feat: added custom route

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -7,11 +7,18 @@ class AlbumsController < ApplicationController
   def show
     album = Album.find_by(id: params[:id])
     if album
-      render json: albums.song,include_songs: true status: :ok
+      render json: album, status: :ok
 
     else
       render json: {error: "album not found"}, status: :not_found
     end
+  end
+
+  def songs
+    # song = find_by(id: params[:id])
+    album = Album.find_by(id: params[:id])
+    album_song = album.songs
+    render json: album_song, status: :ok
   end
 
   private
